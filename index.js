49decea898d63ecd226b1051c0e8a426256d8061
Alun Turner <alun.turner@googlemail.com>
2020-03-17 15:32:58 +0000

    start of project generator

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const { exec } = require("child_process");
+
+fs.mkdir("./newproject", err => {
+  if (err) console.log(err);
+  else {
+    console.log("made a /newproject folder");
+    fs.writeFile(
+      "./newproject/index.js",
+      "// opening the file with a comment",
+      err => {
+        if (err) console.log(err);
+        console.log("made index.js");
+      }
+    );
+    fs.writeFile("./newproject/README.md", "### README FILE ###", err => {
+      if (err) console.log(err);
+      else {
+        console.log("made README.md");
+      }
+    });
+    fs.mkdir("./newproject/spec", err => {
+      if (err) console.log(err);
+      else {
+        console.log("made a /newproject/spec folder");
+        fs.writeFile(
+          "./newproject/spec/index.spec.js",
+          "// this will be for testing",
+          err => {
+            if (err) console.log(err);
+            else {
+              console.log("made index.spec.js");
+            }
+          }
+        );
+      }
+    });
+    exec("npm init -y", { cwd: "./newproject" }, (err, stdout, stderr) => {
+      if (err) console.log(err);
+      console.log("npm init complete");
+    });
+    exec("git init", { cwd: "./newproject" }, (err, stdout, stderr) => {
+      if (err) console.log(err);
+      console.log("git init complete");
+    });
+  }
+});
